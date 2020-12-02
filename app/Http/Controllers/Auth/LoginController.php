450a583eb4827b95394d6d622700990c81f980bc
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2020-12-02 01:45:33 +0100

    Initial commit

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    public function getToken(Request $request)
+    {
+
+        $request->request->add([
+            'grant_type' => 'password',
+            'client_id' => 2,
+            'client_secret' => 'LTVOkhxP85FjrMdSF321ia6bYwcfTOc7pSxNCKFm',
+            'username' => $request->username,
+            'password' => $request->password,
+        ]);
+
+
+        $requestToken = Request::create(env('APP_URL') . '/oauth/token', 'post');
+        $response = Route::dispatch($requestToken);
+
+        return $response;
+    }
+}
