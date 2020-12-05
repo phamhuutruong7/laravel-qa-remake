d094d8467eb23d7042fa3548b5c8fe127c59bfb3
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2020-12-05 23:32:08 +0100

    Fixing authorization.
    Previously, if user logged in, the Edit and Delete button do not appear.
    Reason is policies.js does not understand about model.user_id.
    2 solutions for this.
    1. repair in QuestionResources and add return user_id.
    2. fix it to user.id. because user id is already stick with question.

--- public/js/app.js
+++ public/js/app.js
@@ -12404,10 +12404,6 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-//
-//
-//
-//
 /* harmony default export */ __webpack_exports__["default"] = ({
   props: ['question'],
   methods: {
@@ -66851,9 +66847,6 @@ var render = function() {
                   attrs: { method: "post", action: "#" }
                 },
                 [
-                  _vm._v(
-                    "\n                    @method('DELETE')\n                    @csrf\n                    "
-                  ),
                   _c(
                     "button",
                     {
@@ -82319,13 +82312,13 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
 __webpack_require__.r(__webpack_exports__);
 /* harmony default export */ __webpack_exports__["default"] = ({
   modify: function modify(user, model) {
-    return user.id === model.user_id;
+    return user.id === model.user.id;
   },
   accept: function accept(user, answer) {
-    return user.id === answer.question.user_id;
+    return user.id === answer.question.user.id;
   },
   deleteQuestion: function deleteQuestion(user, question) {
-    return user.id === question.user_id && question.answers_count < 1;
+    return user.id === question.user.id && question.answers_count < 1;
   }
 });
 
