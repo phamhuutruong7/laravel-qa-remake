56852961c55d9f960feb5a41e7db9d3bac0066b6
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2020-12-08 02:52:46 +0100

    Repair small thing
    That MustVerifyEmail cause a problem when user want to register. In this version the email verify will not be setup.

--- app/User.php
+++ app/User.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Passport\HasApiTokens;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable
 {
     use Notifiable, HasApiTokens;
 
